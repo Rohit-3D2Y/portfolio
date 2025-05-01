2fa5aeb5d022f3f6644b9620e104618bec4a265d
Rohit tiwari <rt30082004@gmail.com>
2025-05-01 12:38:56 +0530

    last changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,14 @@ import Work from './components_/Work'
 import Showcase from './components_/Showcase'
 import Expertise from './components_/Expertise'
 import Contact from './components_/Contact'
+import { Analytics } from "@vercel/analytics/react"
+
+
 const App = () => {
   return (
     <>
-    {/* <SplashCursor /> */}
-   
+    <SplashCursor />
+    <Analytics />
     <div className='px-0 md:px-[10%] lg:px-[25%]'>
        <div className='border-r-2 border-l-2 border-slate-200  md:px-10'>
         <div className='p-2'>
